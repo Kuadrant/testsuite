c961dfc55cf5c1789f7d8983acee9d5ad96c5279
phala <phala@redhat.com>
2022-06-07 13:11:38 +0200

    Add secondary check to deploy test to verify that Authorino is ready

--- testsuite/tests/authorino/operator/test_deploy_authorino.py
+++ testsuite/tests/authorino/operator/test_deploy_authorino.py
@@ -4,4 +4,5 @@
 def test_authorino_ready(authorino):
     """Test authorino deploy readiness"""
     status = authorino.deployment.model.status
+    assert all(x.status == "True" for x in authorino.model.status.conditions)
     assert "readyReplicas" in status, f"Deployment is not ready: {status}"
