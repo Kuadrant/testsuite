381a28acb72c006dcb1be6c32ac4ce247eed023d
phala <phala@redhat.com>
2022-06-22 10:23:11 +0200

    Enable running on already deployed Authorino

--- testsuite/tests/kuadrant/authorino/operator/conftest.py
+++ testsuite/tests/kuadrant/authorino/operator/conftest.py
@@ -5,8 +5,11 @@ from testsuite.openshift.objects.authorino import AuthorinoCR
 
 
 @pytest.fixture(scope="session")
-def authorino(openshift, blame, request) -> AuthorinoCR:
+def authorino(openshift, blame, request, testconfig) -> AuthorinoCR:
     """Custom deployed Authorino instance"""
+    if not testconfig["authorino"]["deploy"]:
+        return pytest.skip("Operator tests don't work with already deployed Authorino")
+
     authorino = AuthorinoCR.create_instance(openshift, blame("authorino"))
     request.addfinalizer(lambda: authorino.delete(ignore_not_found=True))
     authorino.commit()
