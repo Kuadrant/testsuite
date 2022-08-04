ce486c450c9f91bca4ca2179c44efd6d49203cd3
phala <phala@redhat.com>
2022-08-04 15:43:55 +0200

    Add remove host test

---
+++ testsuite/tests/kuadrant/authorino/identity/rhsso/multiple_hosts/test_remove_host.py
@@ -0,0 +1,18 @@
+"""Test host removal"""
+
+
+def test_removing_host(client, client2, auth, authorization, second_hostname):
+    """Tests that after removal of the second host, it stops working, while the first one still works"""
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 200
+
+    response = client2.get("/get", auth=auth)
+    assert response.status_code == 200
+
+    authorization.remove_host(second_hostname)
+
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 200
+
+    response = client2.get("/get", auth=auth)
+    assert response.status_code == 404
