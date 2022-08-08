e160d5f6893ed6c86862c591fc359e7d113cf280
phala <phala@redhat.com>
2022-08-08 09:31:47 +0200

    Add test for cluster-wide Authorino

---
+++ testsuite/tests/kuadrant/authorino/operator/clusterwide/test_clusterwide.py
@@ -0,0 +1,16 @@
+"""Test that Authorino can reconcile resources in multiple namespaces"""
+import pytest
+
+
+@pytest.mark.parametrize("client_fixture", [
+    pytest.param("client", id="First namespace"),
+    pytest.param("client2", id="Second namespace"),
+])
+def test_auth(request, client_fixture, auth):
+    """Tests that both AuthConfigs were reconciled in both namespaces"""
+    client = request.getfixturevalue(client_fixture)
+    response = client.get("/get")
+    assert response.status_code == 401
+
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 200
