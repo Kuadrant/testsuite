3935dac31694254c0f4913a58cb6f7014d2e2455
Jakub Urban <jaurban@redhat.com>
2022-08-19 10:25:37 +0200

    Add test for authorino sharding

--- testsuite/tests/kuadrant/authorino/operator/conftest.py
+++ testsuite/tests/kuadrant/authorino/operator/conftest.py
@@ -23,13 +23,16 @@ def authorino(openshift, blame, request, testconfig, cluster_wide, module_label,
     if not testconfig["authorino"]["deploy"]:
         return pytest.skip("Operator tests don't work with already deployed Authorino")
 
-    parameters = {"label_selectors": [f"testRun={module_label}"],
-                  **authorino_parameters}
+    if authorino_parameters.get("label_selectors"):
+        authorino_parameters["label_selectors"].append(f"testRun={module_label}")
+    else:
+        authorino_parameters["label_selectors"] = [f"testRun={module_label}"]
+
     authorino = AuthorinoCR.create_instance(openshift,
                                             blame("authorino"),
                                             cluster_wide=cluster_wide,
                                             image=weakget(testconfig)["authorino"]["image"] % None,
-                                            **parameters)
+                                            **authorino_parameters)
     request.addfinalizer(lambda: authorino.delete(ignore_not_found=True))
     authorino.commit()
     authorino.wait_for_ready()
