12ba323a4304ac356c67ac129a2a9db766be756f
phala <phala@redhat.com>
2022-08-23 09:17:49 +0200

    Add openshift_loader.py

---
+++ testsuite/config/openshift_loader.py
@@ -0,0 +1,21 @@
+"""Custom dynaconf loader for loading OpenShift settings and converting them to OpenshiftClients"""
+from weakget import weakget
+
+from testsuite.openshift.client import OpenShiftClient
+
+
+# pylint: disable=unused-argument
+def load(obj, env=None, silent=True, key=None, filename=None):
+    """Creates all OpenShift clients"""
+    config = weakget(obj)
+    section = config["openshift"]
+    client = OpenShiftClient(
+        section["project"] % None,
+        section["api_url"] % None,
+        section["token"] % None)
+    obj["openshift"] = client
+
+    openshift2 = None
+    if "openshift2" in obj and "project" in obj["openshift2"]:
+        openshift2 = client.change_project(obj["openshift2"]["project"])
+    obj["openshift2"] = openshift2
