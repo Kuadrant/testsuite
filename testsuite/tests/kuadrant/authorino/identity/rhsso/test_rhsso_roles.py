1935b642a4fa2c7532af9c4d5956857fe1541cec
Alex Zgabur <azgabur@redhat.com>
2022-08-23 16:48:48 +0200

    Add role authentication test

---
+++ testsuite/tests/kuadrant/authorino/identity/rhsso/test_rhsso_roles.py
@@ -0,0 +1,40 @@
+"""Basic test of role based authentication"""
+import pytest
+from testsuite.httpx.auth import HttpxOidcClientAuth
+
+
+@pytest.fixture(scope="function")
+def user_with_role(rhsso_service_info, realm_role, blame):
+    """Creates new user and adds him into realm_role"""
+    username = blame("someuser")
+    password = blame("password")
+    user_id = rhsso_service_info.realm.create_user(username, password)
+    rhsso_service_info.realm.assign_realm_role(realm_role, user_id)
+    return {"id": user_id, "username": username, "password": password}
+
+
+@pytest.fixture(scope="module")
+def realm_role(rhsso_service_info, blame):
+    """Creates new realm role"""
+    return rhsso_service_info.realm.create_realm_role(blame("role"))
+
+
+@pytest.fixture(scope="module")
+def authorization(authorization, realm_role, blame):
+    """Adds rule, that requires user to be part of realm_role to be allowed access."""
+    authorization.add_role_rule(blame("rule"), realm_role["name"], "^/get")
+    return authorization
+
+
+def test_user_with_role(client, user_with_role, rhsso_service_info):
+    """Test request when user does have required role using new user with assigned role"""
+    auth = HttpxOidcClientAuth(rhsso_service_info.client, "authorization",
+                               user_with_role["username"], user_with_role["password"])
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 200
+
+
+def test_user_without_role(client, auth):
+    """Test request when user doesn't have required role using default user"""
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 403
