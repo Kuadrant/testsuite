c56217863a090feb641bbb5d0a9db83a15c95bfa
Martin Kudlej <mkudlej@redhat.com>
2022-09-07 15:10:23 +0200

    add port parameter to openshift.types.routes

--- testsuite/tests/kuadrant/authorino/operator/http/conftest.py
+++ testsuite/tests/kuadrant/authorino/operator/http/conftest.py
@@ -7,7 +7,7 @@ from testsuite.httpx import HttpxBackoffClient
 
 # pylint: disable=unused-argument
 @pytest.fixture(scope="module")
-def authorization(authorization, wildcard_domain, blame, openshift, rhsso_service_info, module_label) -> Authorization:
+def authorization(authorization, wildcard_domain, openshift, rhsso_service_info, module_label) -> Authorization:
     """In case of Authorino, AuthConfig used for authorization"""
     authorization.remove_all_hosts()
     authorization.add_host(wildcard_domain)
@@ -21,7 +21,7 @@ def authorization(authorization, wildcard_domain, blame, openshift, rhsso_servic
 
 
 @pytest.fixture(scope="module")
-def client_http_auth(authorization, authorino_route):
+def client(authorization, authorino_route):
     """Returns httpx client to be used for requests, it also commits AuthConfig"""
     client = HttpxBackoffClient(base_url=f"http://{authorino_route.model.spec.host}", verify=False)
     yield client
@@ -31,8 +31,7 @@ def client_http_auth(authorization, authorino_route):
 @pytest.fixture(scope="module")
 def authorino_route(authorino, blame, openshift):
     """Add route for authorino http port to be able to access it."""
-    name = f"route-{authorino.name()}"
-    route = openshift.routes.expose(name, f"{authorino.name()}-authorino-authorization",
+    route = openshift.routes.expose(blame('route'), f"{authorino.name()}-authorino-authorization",
                                     port='http')
     yield route
     route.delete()
