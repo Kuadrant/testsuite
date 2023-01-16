5fbcaf968e8cb0cfcca40e391ed52e348dc79704
phala <phala@redhat.com>
2023-01-16 13:54:49 +0100

    Add basic Limitador tests
    - Simple limit test
    - Multiple iteration test

---
+++ testsuite/tests/kuadrant/limitador/conftest.py
@@ -0,0 +1,17 @@
+"""Conftest for all limitador tests"""
+import pytest
+
+
+@pytest.fixture(scope="module")
+def kuadrant(kuadrant):
+    """Skip if not running on Kuadrant"""
+    if not kuadrant:
+        pytest.skip("Limitador test can only run on Kuadrant for now")
+    return kuadrant
+
+
+@pytest.fixture(scope="module", autouse=True)
+def commit(request, rate_limit):
+    """Commits all important stuff before tests"""
+    request.addfinalizer(rate_limit.delete)
+    rate_limit.commit()
