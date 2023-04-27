f6e51dec14746440024c9913cbdc710fc9a4e728
Alex Zgabur <azgabur@redhat.com>
2023-04-27 20:00:07 +0200

    Add multi-element JSON parsing test

---
+++ testsuite/tests/kuadrant/authorino/metadata/test_multi_element_json.py
@@ -0,0 +1,41 @@
+"""
+Test for checking if multi-element JSON gets parsed correctly.
+https://github.com/Kuadrant/authorino/pull/376
+"""
+
+import pytest
+
+from testsuite.utils import ContentType, extract_from_response
+
+
+MULTI_ELEMENT_JSON = '{"foo": "bar"}\n{"blah": "bleh"}'
+
+
+@pytest.fixture(scope="module")
+def json_mock_expectation(request, mockserver, module_label):
+    """Creates Mockserver Expectation which returns multi-element JSON."""
+    request.addfinalizer(lambda: mockserver.clear_expectation(module_label))
+    return mockserver.create_expectation(module_label, MULTI_ELEMENT_JSON, ContentType.APPLICATION_JSON)
+
+
+@pytest.fixture(scope="module")
+def authorization(authorization, json_mock_expectation):
+    """
+    Adds auth metadata HTTP endpoint and header 'Auth-Json' inspecting parsed metadata value.
+    """
+    authorization.metadata.http_metadata("mock", json_mock_expectation, "GET")
+    authorization.responses.add(
+        {
+            "name": "auth-json",
+            "json": {"properties": [{"name": "mock", "valueFrom": {"authJSON": "auth.metadata.mock"}}]},
+        }
+    )
+    return authorization
+
+
+def test_metadata_contents(client, auth):
+    """This test exports parsed metadata value from headers and checks if it is a list of size two."""
+    response = client.get("/get", auth=auth)
+    extracted = extract_from_response(response, section="mock")
+    assert isinstance(extracted, list)
+    assert len(extracted) == 2
