b183db132fd3526ab774f886e14187e7ae53ca98
averevki <sandyverevkin@gmail.com>
2023-08-02 23:10:35 +0200

    Add prometheus metrics client

---
+++ testsuite/openshift/objects/metrics/__init__.py
@@ -0,0 +1,5 @@
+"""Package implements objects to work with metrics from Prometheus client for OpenShift"""
+from .prometheus import Prometheus, Metrics
+from .service_monitor import ServiceMonitor, MetricsEndpoint
+
+__all__ = ["Prometheus", "Metrics", "ServiceMonitor", "MetricsEndpoint"]
