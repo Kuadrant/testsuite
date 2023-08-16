bf65ef6cbb55556a545b00776c2572c18a1f85c0
Alex Zgabur <azgabur@redhat.com>
2023-08-16 12:36:48 +0200

    Add asdict_compose function for composed dataclasses

--- testsuite/objects/__init__.py
+++ testsuite/objects/__init__.py
@@ -1,27 +1,27 @@
 """Module containing base classes for common objects"""
 import abc
-from dataclasses import dataclass, is_dataclass, fields
+from dataclasses import dataclass, is_dataclass, fields, replace
 from copy import deepcopy
 from typing import Literal, Union
 
 JSONValues = Union[None, str, int, bool, list["JSONValues"], dict[str, "JSONValues"]]
 
 
-def asdict(obj) -> dict[str, JSONValues]:
+def asdict(obj, ignore_overwrite=False) -> dict[str, JSONValues]:
     """
     This function converts dataclass object to dictionary.
-    While it works similar to `dataclasses.asdict` a notable change is usage of
-    overriding `to_dict()` function if dataclass contains it.
+    While it works similar to `dataclasses.asdict` a notable change is usage of overriding `asdict_override()` function
+    if dataclass contains it and `ignore_overwrite` is set to False (it is by default).
     This function works recursively in lists, tuples and dicts. All other values are passed to copy.deepcopy function.
     """
     if not is_dataclass(obj):
         raise TypeError("asdict() should be called on dataclass instances")
-    return _asdict_recurse(obj)
+    return _asdict_recurse(obj, ignore_overwrite)
 
 
-def _asdict_recurse(obj):
-    if hasattr(obj, "asdict"):
-        return obj.asdict()
+def _asdict_recurse(obj, ignore_overwrite=False):
+    if hasattr(obj, "asdict_override") and not ignore_overwrite:
+        return obj.asdict_override()
 
     if not is_dataclass(obj):
         return deepcopy(obj)
@@ -40,6 +40,23 @@ def _asdict_recurse(obj):
             result[field.name] = type(value)((_asdict_recurse(k), _asdict_recurse(v)) for k, v in value.items())
         else:
             result[field.name] = deepcopy(value)
+    return result
+
+
+def asdict_compose(obj):
+    """
+    Helper asdict function for classes which use class composition and want to keep flat dictionary structure.
+    This removes the key under which the dataclass object was stored under and replaces it with
+    the dictionary view of the dataclass object.
+    For use in asdict_override() function.
+    """
+    obj_copy = replace(obj)
+    result = {}
+    for field in fields(obj_copy):
+        if is_dataclass(field):
+            result.update(asdict(field))
+            delattr(obj_copy, field.name)
+    result.update(asdict(obj_copy, ignore_overwrite=True))
     return result
 
 
@@ -92,7 +109,7 @@ class ValueFrom(ABCValue):
 
     authJSON: str  # pylint: disable=invalid-name
 
-    def asdict(self):
+    def asdict_override(self):
         """Override `asdict` function"""
         return {"valueFrom": {"authJSON": self.authJSON}}
 
@@ -104,9 +121,9 @@ class Property:
     name: str
     value: ABCValue
 
-    def asdict(self):
+    def asdict_override(self):
         """Override `asdict` function"""
-        return {"name": self.name, **asdict(self.value)}
+        return asdict_compose(self)
 
 
 @dataclass
