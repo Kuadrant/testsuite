b4ce71789228794e8d09c0636eea12b99c6ed7b6
phala <phala@redhat.com>
2023-10-18 11:09:17 +0200

    Improve metrics tests skip
    - Now it should skip if the userWorkloadMonitoring is off
    - Add ConfigMap class for ease of use

--- testsuite/tests/kuadrant/authorino/metrics/conftest.py
+++ testsuite/tests/kuadrant/authorino/metrics/conftest.py
@@ -1,6 +1,10 @@
 """Conftest for the Authorino metrics tests"""
-import pytest
+import yaml
 
+import pytest
+from openshift import selector
+
+from testsuite.openshift.objects.config_map import ConfigMap
 from testsuite.openshift.objects.metrics import ServiceMonitor, MetricsEndpoint, Prometheus
 
 
@@ -16,9 +20,18 @@ def prometheus(request, openshift):
     Return an instance of OpenShift metrics client
     Skip tests if query route is not properly configured
     """
+    openshift_monitoring = openshift.change_project("openshift-monitoring")
+    # Check if metrics are enabled
+    try:
+        with openshift_monitoring.context:
+            cm = selector("cm/cluster-monitoring-config").object(cls=ConfigMap)
+            assert yaml.safe_load(cm["config.yaml"])["enableUserWorkload"]
+    except Exception:  # pylint: disable=broad-exception-caught
+        pytest.skip("User workload monitoring is disabled")
+
     # find thanos-querier route in the openshift-monitoring project
     # this route allows to query metrics
-    openshift_monitoring = openshift.change_project("openshift-monitoring")
+
     routes = openshift_monitoring.get_routes_for_service("thanos-querier")
     if len(routes) > 0:
         url = ("https://" if "tls" in routes[0].model.spec else "http://") + routes[0].model.spec.host
