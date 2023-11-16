a8dddaed0b39d67054893ed6b9fe664ba44f5f29
phala <phala@redhat.com>
2023-11-16 14:44:46 +0100

    Enhance wristband test
    - Use standard client in test methods
    - Improve fixture naming

--- testsuite/tests/kuadrant/authorino/wristband/test_wristband.py
+++ testsuite/tests/kuadrant/authorino/wristband/test_wristband.py
@@ -14,13 +14,13 @@ def test_wristband_token_claims(oidc_provider, auth, wristband_token, wristband_
         assert claim not in wristband_decoded
 
 
-def test_wristband_success(authenticated_client, wristband_token):
+def test_wristband_success(client, wristband_token):
     """Test api authentication with token that was acquired after successful authentication in the edge"""
-    response = authenticated_client.get("/get", headers={"Authorization": "Bearer " + wristband_token})
+    response = client.get("/get", headers={"Authorization": "Bearer " + wristband_token})
     assert response.status_code == 200
 
 
-def test_wristband_fail(authenticated_client, auth):
+def test_wristband_fail(client, auth):
     """Test api authentication with token that only accepted in the edge"""
-    response = authenticated_client.get("/get", auth=auth)  # oidc access token instead of wristband
+    response = client.get("/get", auth=auth)  # oidc access token instead of wristband
     assert response.status_code == 401
