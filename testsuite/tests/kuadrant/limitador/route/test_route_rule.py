22557992cdc5737ae4f72ece47a29579c1e0adfe
Jakub Smolar <jsmolar@redhat.com>
2024-02-08 12:09:10 +0100

    Add tests for RLP and rule mismatch

--- testsuite/tests/kuadrant/limitador/route/test_route_rule.py
+++ testsuite/tests/kuadrant/limitador/route/test_route_rule.py
@@ -11,12 +11,15 @@ pytestmark = [pytest.mark.kuadrant_only, pytest.mark.limitador]
 @pytest.fixture(scope="module")
 def rate_limit(rate_limit):
     """Add limit to the policy"""
-    selector = RouteSelector(RouteMatch(path=PathMatch(value="/anything", type=MatchType.PATH_PREFIX)))
+    selector = RouteSelector(
+        RouteMatch(path=PathMatch(value="/get", type=MatchType.PATH_PREFIX)),
+        RouteMatch(path=PathMatch(value="/anything/test", type=MatchType.PATH_PREFIX)),
+    )
     rate_limit.add_limit("multiple", [Limit(5, 10)], route_selectors=[selector])
     return rate_limit
 
 
-def test_rule(client):
+def test_rule_match(client):
     """Tests that RLP correctly applies to the given HTTPRoute rule"""
     responses = client.get_many("/get", 5)
     responses.assert_all(status_code=200)
