2ce36fefb8fec3230940827d91e1be4e262b1175
Jakub Smolar <jsmolar@redhat.com>
2024-02-26 15:04:34 +0100

    Fixed caching tests

--- testsuite/tests/kuadrant/authorino/caching/metadata/test_ttl.py
+++ testsuite/tests/kuadrant/authorino/caching/metadata/test_ttl.py
@@ -1,4 +1,6 @@
-"""Tests metadata caching"""
+"""Tests TTL for metadata caching"""
+
+from time import sleep
 
 import pytest
 
@@ -9,25 +11,28 @@ pytestmark = [pytest.mark.authorino]
 
 
 @pytest.fixture(scope="module")
-def authorization(authorization, module_label, expectation_path):
+def cache_ttl():
+    """Returns TTL in seconds for Cached Metadata"""
+    return 5
+
+
+@pytest.fixture(scope="module")
+def authorization(authorization, module_label, expectation_path, cache_ttl):
     """Adds Cached Metadata to the AuthConfig"""
-    meta_cache = Cache(5, ValueFrom("context.request.http.path"))
+    meta_cache = Cache(cache_ttl, ValueFrom("context.request.http.path"))
     authorization.metadata.add_http(module_label, expectation_path, "GET", cache=meta_cache)
     return authorization
 
 
-def test_cached(client, auth, module_label, mockserver):
-    """
-    Tests cache with two subsequent requests:
-        - both requests return the same result
-        - only single external value evaluation occurs. The second response contains cached (in-memory) value
-    """
+def test_cached_ttl(client, auth, module_label, cache_ttl, mockserver):
+    """Tests that cached value expires after ttl"""
     response = client.get("/get", auth=auth)
     data = extract_response(response)[module_label]["uuid"] % None
+    sleep(cache_ttl)
     response = client.get("/get", auth=auth)
     cached_data = extract_response(response)[module_label]["uuid"] % None
 
     assert cached_data is not None
     assert data is not None
-    assert data == cached_data
-    assert len(mockserver.retrieve_requests(module_label)) == 1
+    assert data != cached_data
+    assert len(mockserver.retrieve_requests(module_label)) == 2
