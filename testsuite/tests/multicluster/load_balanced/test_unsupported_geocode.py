690c30a209fc94eeb7754579ca7de0807fca946f
averevki <sandyverevkin@gmail.com>
2024-09-18 15:33:30 +0200

    Improve assert on unsupported geo test
    Signed-off-by: averevki <sandyverevkin@gmail.com>

--- testsuite/tests/multicluster/load_balanced/test_unsupported_geocode.py
+++ testsuite/tests/multicluster/load_balanced/test_unsupported_geocode.py
@@ -3,6 +3,7 @@
 import pytest
 
 from testsuite.kuadrant.policy import has_condition
+from testsuite.kuadrant.policy.dns import has_record_condition
 
 pytestmark = [pytest.mark.multicluster]
 
@@ -13,3 +14,6 @@ def test_unsupported_geocode(dns_policy):
     dns_policy.apply()
 
     assert dns_policy.wait_until(has_condition("Enforced", "False"))
+    assert dns_policy.wait_until(
+        has_record_condition("Ready", "False", "ProviderError", "Cannot find location")
+    ), f"DNSPolicy did not reach expected record status, instead it was: {dns_policy.model.status.recordConditions}"
