1e53594e62db6eb0c6dc13b73f7fc8235c22c4dd
Martin Hesko <martinhesko8@gmail.com>
2024-10-10 17:23:09 +0200

    Removed RouteSelectors due to version bump
    Signed-off-by: Martin Hesko <martinhesko8@gmail.com>

--- testsuite/tests/singlecluster/limitador/route/test_route_rule.py
+++ testsuite/tests/singlecluster/limitador/route/test_route_rule.py
@@ -2,8 +2,8 @@
 
 import pytest
 
-from testsuite.gateway import RouteMatch, PathMatch, MatchType
-from testsuite.kuadrant.policy.rate_limit import Limit, RouteSelector
+from testsuite.kuadrant.policy.rate_limit import Limit
+from testsuite.kuadrant.policy.authorization import Pattern
 
 pytestmark = [pytest.mark.kuadrant_only, pytest.mark.limitador]
 
@@ -11,14 +11,12 @@ pytestmark = [pytest.mark.kuadrant_only, pytest.mark.limitador]
 @pytest.fixture(scope="module")
 def rate_limit(rate_limit):
     """Add limit to the policy"""
-    selector = RouteSelector(
-        RouteMatch(path=PathMatch(value="/get", type=MatchType.PATH_PREFIX)),
-        RouteMatch(path=PathMatch(value="/anything/test", type=MatchType.PATH_PREFIX)),
-    )
-    rate_limit.add_limit("multiple", [Limit(5, 10)], route_selectors=[selector])
+    when = [Pattern("request.path", "eq", "/get")]
+    rate_limit.add_limit("multiple", [Limit(5, 10)], when=when)
     return rate_limit
 
 
+@pytest.mark.issue("https://github.com/Kuadrant/testsuite/issues/561")
 def test_rule_match(client):
     """Tests that RLP correctly applies to the given HTTPRoute rule"""
     responses = client.get_many("/get", 5)
@@ -28,9 +26,3 @@ def test_rule_match(client):
 
     response = client.get("/anything")
     assert response.status_code == 200
-
-
-def test_rule_missmatch(client):
-    """Tests that RLP is not applied for not existing route rule"""
-    responses = client.get_many("/anything/test", 7)
-    responses.assert_all(status_code=200)
