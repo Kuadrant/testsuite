9301b5cb630c3a2bb05a46cebf774fd80aa90196
Martin Hesko <martinhesko8@gmail.com>
2024-11-14 15:04:11 +0100

    Modified overrides tests to adjust to overrides being applicable to HTTPRoutes now
    Signed-off-by: Martin Hesko <martinhesko8@gmail.com>

--- testsuite/tests/singlecluster/overrides/test_basic_rate_limit.py
+++ testsuite/tests/singlecluster/overrides/test_basic_rate_limit.py
@@ -1,4 +1,4 @@
-"""Test basic enforcement of the rules inside the 'overrides' block of the RateLimitPolicy assigned to a Gateway"""
+"""Test enforcement of the rules inside the 'overrides' block of the RateLimitPolicy assigned to a Gateway/HTTPRoute"""
 
 import pytest
 
@@ -6,7 +6,7 @@ from testsuite.kuadrant.policy.rate_limit import Limit, RateLimitPolicy
 
 pytestmark = [pytest.mark.kuadrant_only, pytest.mark.limitador]
 
-GATEWAY_LIMIT = Limit(3, "5s")
+OVERRIDE_LIMIT = Limit(3, "5s")
 ROUTE_LIMIT = Limit(2, "5s")
 
 
@@ -16,33 +16,34 @@ def authorization():
     return None
 
 
-@pytest.fixture(scope="module")
-def rate_limit_gw(request, cluster, blame, module_label, gateway):
-    """Add a RateLimitPolicy to the Gateway with an overrides block to override the Route-level policy."""
-    rate_limit_gateway = RateLimitPolicy.create_instance(
-        cluster, blame("limit-gateway"), gateway, labels={"testRun": module_label}
+@pytest.fixture(scope="function")
+def rate_limit_route(request, cluster, blame, module_label, route):
+    """Add a RateLimitPolicy to the HTTPRoute with a basic limit to be overriden."""
+    rate_limit_route = RateLimitPolicy.create_instance(
+        cluster, blame("limit-route"), route, labels={"testRun": module_label}
     )
-    rate_limit_gateway.overrides.add_limit("basic", [GATEWAY_LIMIT])
-    request.addfinalizer(rate_limit_gateway.delete)
-    rate_limit_gateway.commit()
-    rate_limit_gateway.wait_for_ready()
-    return rate_limit_gateway
+    rate_limit_route.add_limit("basic", [ROUTE_LIMIT])
+    request.addfinalizer(rate_limit_route.delete)
+    rate_limit_route.commit()
+    rate_limit_route.wait_for_accepted()
+    return rate_limit_route
 
 
 @pytest.fixture(scope="module")
 def rate_limit(rate_limit):
-    """Add basic requests limit to RateLimitPolicy"""
-    rate_limit.add_limit("basic", [ROUTE_LIMIT])
+    """Add an override to RateLimitPolicy"""
+    rate_limit.overrides.add_limit("override-limit", [OVERRIDE_LIMIT])
     return rate_limit
 
 
-def test_basic_rate_limit(rate_limit, rate_limit_gw, route, client):
-    """Test if rules inside overrides block of Gateway's RateLimitPolicy are inherited by the HTTPRoute
-    and enforced like any other normal rule"""
+@pytest.mark.parametrize("rate_limit", ["route", "gateway"], indirect=True)
+def test_basic_rate_limit(rate_limit, rate_limit_route, route, client):
+    """Test if rules inside overrides block of Gateway/HTTPRoute RateLimitPolicy are inherited by the HTTPRoute
+    and override the rate limit targeting the route."""
     route.refresh()
     assert route.is_affected_by(rate_limit)
-    rate_limit_gw.wait_for_full_enforced()
+    assert route.is_affected_by(rate_limit_route)
 
-    responses = client.get_many("/get", GATEWAY_LIMIT.limit)
+    responses = client.get_many("/get", OVERRIDE_LIMIT.limit)
     responses.assert_all(status_code=200)
     assert client.get("/get").status_code == 429  # assert that RateLimitPolicy is enforced
