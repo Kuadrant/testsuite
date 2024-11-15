b6aedca7c7a3d6dbee3a71a048f65e062d5ed352
Jakub Smolar <jsmolar@redhat.com>
2024-11-15 13:31:01 +0100

    Fix metadata tests
    Signed-off-by: Jakub Smolar <jsmolar@redhat.com>

--- testsuite/tests/singlecluster/authorino/metadata/conftest.py
+++ testsuite/tests/singlecluster/authorino/metadata/conftest.py
@@ -6,16 +6,14 @@ from testsuite.kubernetes import KubernetesObject
 
 
 @pytest.fixture(scope="module")
-def create_client_secret(request, cluster):
+def create_client_secret(request, cluster, authorino):
     """Creates Client Secret, used by Authorino to start the authentication with the UMA registry"""
 
     def _create_secret(name, client_id, client_secret):
         model = {
             "apiVersion": "v1",
             "kind": "Secret",
-            "metadata": {
-                "name": name,
-            },
+            "metadata": {"name": name, "namespace": authorino.namespace()},
             "stringData": {"clientID": client_id, "clientSecret": client_secret},
             "type": "Opaque",
         }
