4826e2216a3b1b4ce8bd707a28dc4b7481ea3c69
Jakub Smolar <jsmolar@redhat.com>
2024-11-21 12:54:47 +0100

    Add temporar standalone_only mark to deep metrics test
    Signed-off-by: Jakub Smolar <jsmolar@redhat.com>

--- testsuite/tests/singlecluster/authorino/metrics/test_deep_metrics.py
+++ testsuite/tests/singlecluster/authorino/metrics/test_deep_metrics.py
@@ -4,7 +4,7 @@ import pytest
 
 from testsuite.kuadrant.policy.authorization import Value, JsonResponse
 
-pytestmark = [pytest.mark.authorino]
+pytestmark = [pytest.mark.authorino, pytest.mark.standalone_only]
 
 
 @pytest.fixture(scope="module")
