430cc7a3140647e6edc0626b4aac546814a7be1e
Tomas Repel <trepel@redhat.com>
2024-11-25 14:02:43 +0100

    Removed redundant RLP removal, rename of test files
    Signed-off-by: Tomas Repel <trepel@redhat.com>

--- testsuite/tests/singlecluster/identical_hostnames/auth/conftest.py
+++ testsuite/tests/singlecluster/identical_hostnames/auth/conftest.py
@@ -8,11 +8,3 @@ def authorization(authorization):
     """1st 'allow-all' Authorization object"""
     authorization.authorization.add_opa_policy("rego", "allow = true")
     return authorization
-
-
-@pytest.fixture(scope="module")
-def rate_limit():
-    """
-    For these tests RateLimitPolicy is not required
-    """
-    return None
