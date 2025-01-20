4265635d7df158ac72acd686adb7eeb361a636dd
averevki <sandyverevkin@gmail.com>
2025-01-20 20:34:01 +0100

    Fix unsynced objects in multicluster tests
    Signed-off-by: averevki <sandyverevkin@gmail.com>

--- testsuite/tests/multicluster/load_balanced/test_unsupported_geocode.py
+++ testsuite/tests/multicluster/load_balanced/test_unsupported_geocode.py
@@ -10,8 +10,9 @@ pytestmark = [pytest.mark.multicluster]
 
 def test_unsupported_geocode(dns_policy2):
     """Change default geocode to not existent one and verify that policy became not enforced"""
-    dns_policy2.model.spec.loadBalancing.geo = "XX"
-    dns_policy2.apply()
+    dns_policy2.refresh().model.spec.loadBalancing.geo = "XX"
+    res = dns_policy2.apply()
+    assert res.status() == 0, res.err()
 
     assert dns_policy2.wait_until(has_condition("Enforced", "False"))
     assert dns_policy2.wait_until(
