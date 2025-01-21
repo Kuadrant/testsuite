86e4d8fce04d3bfbc468a5ec751c2aaeb2e30546
emmaaroche <eroche@redhat.com>
2025-01-21 14:28:14 +0000

    Test for changing targetRef field in policies (AuthPolicy and RateLimitPolicy)
    Signed-off-by: emmaaroche <eroche@redhat.com>

---
+++ testsuite/tests/singlecluster/gateway/reconciliation/change_targetref/test_update_authpolicy_target_ref.py
@@ -0,0 +1,39 @@
+"""
+Test for changing targetRef field in AuthPolicy
+"""
+
+import pytest
+
+from testsuite.kuadrant.policy.authorization.auth_policy import AuthPolicy
+
+pytestmark = [pytest.mark.kuadrant_only, pytest.mark.dnspolicy]
+
+
+@pytest.fixture(scope="module")
+def authorization(oidc_provider, gateway, cluster, blame, module_label, route):  # pylint: disable=unused-argument
+    """Overwrite the authorization fixture and attach it to the gateway"""
+    policy = AuthPolicy.create_instance(cluster, blame("authz"), gateway, labels={"testRun": module_label})
+    policy.identity.add_oidc("default", oidc_provider.well_known["issuer"])
+    return policy
+
+
+def test_update_auth_policy_target_ref(
+    gateway2, authorization, client, client2, auth, dns_policy, dns_policy2, change_target_ref
+):  # pylint: disable=unused-argument
+    """Test updating the targetRef of an AuthPolicy from Gateway 1 to Gateway 2"""
+    response = client.get("/get", auth=auth)
+    assert response.status_code == 200
+
+    response = client.get("/get")
+    assert response.status_code == 401
+
+    response = client2.get("/get")
+    assert response.status_code == 200
+
+    change_target_ref(authorization, gateway2)
+
+    response = client2.get("/get", auth=auth)
+    assert response.status_code == 200
+
+    response = client2.get("/get")
+    assert response.status_code == 401
