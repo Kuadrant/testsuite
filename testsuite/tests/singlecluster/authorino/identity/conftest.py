da73c71fdd8f6509725497d18877f296196c265c
averevki <sandyverevkin@gmail.com>
2025-01-28 18:03:00 +0100

    Add test for SubjectAccessReview authorization
    Signed-off-by: averevki <sandyverevkin@gmail.com>

--- testsuite/tests/singlecluster/authorino/identity/conftest.py
+++ testsuite/tests/singlecluster/authorino/identity/conftest.py
@@ -2,6 +2,21 @@
 
 import pytest
 
+from testsuite.kubernetes.service_account import ServiceAccount
+
+
+@pytest.fixture(scope="module")
+def create_service_account(request, cluster, blame, module_label):
+    """Creates and returns service account"""
+
+    def _create_service_account(name):
+        service_account = ServiceAccount.create_instance(cluster, blame(name), labels={"app": module_label})
+        request.addfinalizer(service_account.delete)
+        service_account.commit()
+        return service_account
+
+    return _create_service_account
+
 
 @pytest.fixture(scope="module")
 def authorization(authorization):
