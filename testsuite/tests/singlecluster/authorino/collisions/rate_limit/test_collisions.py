3c38342fa83b94ffe4383a697fcba8022bc859e0
Alexander Cristurean <acristur@redhat.com>
2025-04-01 11:15:17 +0200

    fix linter.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/tests/singlecluster/authorino/collisions/rate_limit/test_collisions.py
+++ testsuite/tests/singlecluster/authorino/collisions/rate_limit/test_collisions.py
@@ -1,12 +1,13 @@
+"""Test rate limit policies collisions when attached to the same target"""
+
 import pytest
 
-from testsuite.httpx.auth import HeaderApiKeyAuth
 from testsuite.kuadrant.policy import has_condition, CelPredicate
-from testsuite.kuadrant.policy.authorization.auth_policy import AuthPolicy
 from testsuite.kuadrant.policy.rate_limit import Limit, RateLimitPolicy
 
 FIRST_LIMIT = Limit(3, "5s")
 SECOND_LIMIT = Limit(6, "5s")
+
 
 # pylint: disable = unused-argument
 @pytest.fixture(scope="module")
@@ -17,6 +18,7 @@ def rate_limit(request, cluster, blame, module_label, route, label):
     rate_limit = RateLimitPolicy.create_instance(cluster, blame("fp"), target_ref, labels={"testRun": module_label})
     rate_limit.add_limit("first", [FIRST_LIMIT], when=[CelPredicate("request.path == '/get'")])
     return rate_limit
+
 
 @pytest.fixture(scope="module")
 def rate_limit2(request, cluster, blame, module_label, route, label):
@@ -36,17 +38,11 @@ def commit(request, rate_limit, rate_limit2):
         policy.commit()
         policy.wait_for_ready()
 
+
 @pytest.mark.parametrize("rate_limit", ["gateway", "route"], indirect=True)
-def test_collision(client, rate_limit, rate_limit2):
+def test_collision_rate_limit(client, rate_limit, rate_limit2):
     """Test first policy is being overridden when another policy with the same target is created."""
-    assert rate_limit.wait_until(
-        has_condition(
-            "Enforced",
-            "False",
-            "Overridden",
-            "RateLimitPolicy is overridden"
-        )
-    )
+    assert rate_limit.wait_until(has_condition("Enforced", "False", "Overridden", "RateLimitPolicy is overridden"))
     responses = client.get_many("/get", FIRST_LIMIT.limit)
     responses.assert_all(status_code=200)
     assert client.get("/get").status_code == 200
