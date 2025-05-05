ad69ceb2e84cdb4409d0095307c23bbc45fe05d0
Alexander Cristurean <acristur@redhat.com>
2025-05-05 13:00:31 +0200

    added generation assertion.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/tests/singlecluster/authorino/identity/anonymous/test_anonymous_identity.py
+++ testsuite/tests/singlecluster/authorino/identity/anonymous/test_anonymous_identity.py
@@ -1,8 +1,12 @@
 """Test for anonymous identity"""
 
+import logging
+
 import pytest
 
 pytestmark = [pytest.mark.authorino]
+
+logger = logging.getLogger(__name__)
 
 
 @pytest.fixture(scope="module")
@@ -10,6 +14,17 @@ def authorization(authorization, keycloak):
     """Add Keycloak identity"""
     authorization.identity.add_oidc("keycloak", keycloak.well_known["issuer"])
     return authorization
+
+
+def has_generation(generation):
+    """Check expected generation is present in the object's definition"""
+
+    def _check(obj):
+        if obj.model.metadata.generation == generation:
+            return True
+        return False
+
+    return _check
 
 
 def test_anonymous_identity(client, auth, authorization):
@@ -31,7 +46,10 @@ def test_anonymous_identity(client, auth, authorization):
     response = client.get("/get")
     assert response.status_code == 401
 
+    generation = authorization.model.metadata.generation + 1
     authorization.identity.add_anonymous("anonymous")
+    success = authorization.wait_until(has_generation(generation))
+    assert success, "AuthPolicy did not increment the generation in due time"
     authorization.wait_for_ready()
 
     response = client.get("/get")
