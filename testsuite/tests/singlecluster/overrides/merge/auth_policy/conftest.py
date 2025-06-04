bacab2496eac519154a8cc2392376c12b60e0f9a
Alexander Cristurean <acristur@redhat.com>
2025-06-04 16:29:35 +0200

    removed cluster fixture.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/tests/singlecluster/overrides/merge/auth_policy/conftest.py
+++ testsuite/tests/singlecluster/overrides/merge/auth_policy/conftest.py
@@ -20,18 +20,18 @@ def admin_label(blame):
 
 
 @pytest.fixture(scope="module")
-def user_api_key(create_api_key, user_label, cluster):
+def user_api_key(create_api_key, user_label):
     """Creates API key Secret for a user"""
     annotations = {"kuadrant.io/groups": "users"}
-    secret = create_api_key("api-key", user_label, "api_key_value", annotations, cluster)
+    secret = create_api_key("api-key", user_label, "api_key_value", annotations=annotations)
     return secret
 
 
 @pytest.fixture(scope="module")
-def admin_api_key(create_api_key, admin_label, cluster):
+def admin_api_key(create_api_key, admin_label):
     """Creates API key Secret for an admin"""
     annotations = {"kuadrant.io/groups": "admins"}
-    secret = create_api_key("admin-api-key", admin_label, "admin_api_key_value", annotations, cluster)
+    secret = create_api_key("admin-api-key", admin_label, "admin_api_key_value", annotations=annotations)
     return secret
 
 
