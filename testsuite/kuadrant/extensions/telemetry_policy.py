1dba55d68c2f5b81eb8daeee07961884a5b773fb
Silvia Tarabova <starabov@redhat.com>
2025-09-24 14:25:20 +0200

    Add class for TelemetryPolicy
    Signed-off-by: Silvia Tarabova <starabov@redhat.com>

---
+++ testsuite/kuadrant/extensions/telemetry_policy.py
@@ -0,0 +1,50 @@
+"""Module containing classes related to TelemetryPolicy"""
+
+from typing import Dict
+
+from testsuite.gateway import Referencable
+from testsuite.kubernetes import modify
+from testsuite.kubernetes.client import KubernetesClient
+from testsuite.kuadrant.policy import Policy
+
+
+class TelemetryPolicy(Policy):
+    """TelemetryPolicy for configuring telemetry metrics labels"""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    @classmethod
+    def create_instance(
+        cls,
+        cluster: KubernetesClient,
+        name: str,
+        target: Referencable,
+        labels: Dict[str, str] = None,
+        section_name: str = None,
+    ):
+        """Creates base instance"""
+        model: Dict = {
+            "apiVersion": "extensions.kuadrant.io/v1alpha1",
+            "kind": "TelemetryPolicy",
+            "metadata": {"name": name, "namespace": cluster.project, "labels": labels},
+            "spec": {
+                "targetRef": target.reference,
+            },
+        }
+        if section_name:
+            model["spec"]["targetRef"]["sectionName"] = section_name
+
+        return cls(model, context=cluster.context)
+
+    @modify
+    def add_label(self, label_name, label_path):
+        """Add a label to the TelemetryPolicy"""
+        metrics = self.model.spec.setdefault("metrics", {})
+        default = metrics.setdefault("default", {})
+        labels = default.setdefault("labels", {})
+        labels[label_name] = label_path
+
+    def wait_for_ready(self):
+        # TODO: Delete this method when TelemetryPolicy status is fixed.
+        pass
