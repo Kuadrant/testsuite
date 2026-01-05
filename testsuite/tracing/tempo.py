15d8c1379700e805adc0349e56502999cc1a7fc8
Silvia Tarabova <starabov@redhat.com>
2026-01-05 11:00:49 +0100

    feat: add distributed tracing support for Kuadrant components
    Signed-off-by: Silvia Tarabova <starabov@redhat.com>

--- testsuite/tracing/tempo.py
+++ testsuite/tracing/tempo.py
@@ -9,9 +9,10 @@ class RemoteTempoClient(JaegerClient):
     """Client to a Tempo that is deployed remotely"""
 
     @backoff.on_predicate(backoff.fibo, lambda x: x == [], max_tries=7, jitter=None)
-    def search(self, request_id: str, service: str, tags=None):
+    def get_trace(self, request_id: str, service: str, tag_name: str, tags=None):
+        """Gets trace from Tempo tracing backend."""
         if tags is None:
             tags = {}
 
-        tags.update({"service.name": service, "authorino.request_id": request_id})
-        return self.query.api.search.get(params=tags).json()["traces"]
+        tags.update({"service.name": service, tag_name: request_id})
+        return self.query.api.get_trace.get(params=tags).json()["traces"]
