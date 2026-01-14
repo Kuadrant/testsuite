016d93441def61130cb54e4a0a9ec48d1b100672
Alexander Cristurean <acristur@redhat.com>
2026-01-14 17:05:09 +0100

    feat: added envoy wait mixin.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/kuadrant/policy/__init__.py
+++ testsuite/kuadrant/policy/__init__.py
@@ -4,7 +4,54 @@ from dataclasses import dataclass
 from enum import Enum
 
 from testsuite.kubernetes import KubernetesObject
+from testsuite.kuadrant.metrics import get_kuadrant_configs_value, wait_for_policy_applied_to_envoy
 from testsuite.utils import check_condition
+
+
+class EnvoyWaitMixin:
+    """Mixin providing Envoy configuration waiting functionality.
+
+    This mixin can be used by any class (Policy, AuthConfig, etc.) that needs to wait
+    for Envoy to actually apply WASM configurations after K8s resources are ready.
+    """
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self._initial_kuadrant_configs = None
+        self._metrics_route = None
+
+    def set_metrics_route(self, metrics_route):
+        """Store metrics route for Envoy waiting. Call before commit().
+
+        Args:
+            metrics_route: OpenShift Route to gateway metrics service
+        """
+        self._metrics_route = metrics_route
+        if metrics_route is not None:
+            self._initial_kuadrant_configs = get_kuadrant_configs_value(metrics_route)
+
+    def wait_for_envoy_applied(self, timeout=120):
+        """Wait for configuration to be actually applied in Envoy (WASM config loaded).
+
+        This should be called after wait_for_ready(). Checks that kuadrant_configs
+        metric increased, indicating Envoy loaded the new WASM configuration.
+
+        Args:
+            timeout: Maximum time to wait in seconds (default: 120)
+
+        Returns:
+            True if applied, False if no metrics_route was set
+        """
+        if self._metrics_route is None:
+            # No metrics route configured, skip waiting
+            return False
+
+        success = wait_for_policy_applied_to_envoy(self._metrics_route, self._initial_kuadrant_configs, timeout=timeout)
+        assert success, (
+            f"{self.__class__.__name__} was ready in K8s but did not get applied in Envoy within {timeout}s "
+            f"(initial kuadrant_configs: {self._initial_kuadrant_configs})"
+        )
+        return True
 
 
 class Strategy(Enum):
@@ -68,7 +115,7 @@ def is_affected_by(policy: "Policy"):
     return _check
 
 
-class Policy(KubernetesObject):
+class Policy(EnvoyWaitMixin, KubernetesObject):
     """Base class with common functionality for all policies"""
 
     def wait_for_ready(self):
@@ -77,6 +124,10 @@ class Policy(KubernetesObject):
         success = self.wait_until(has_observed_generation(self.generation))
         assert success, f"{self.kind()} did not reach observed generation in time"
         self.wait_for_full_enforced()
+
+        # Automatically wait for Envoy application if metrics_route was set
+        if self._metrics_route is not None:
+            self.wait_for_envoy_applied()
 
     def wait_for_accepted(self):
         """Wait for a Policy to be Accepted"""
