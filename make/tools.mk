375cb518d4aa675dbbcc59b31ada67e2d3680b3f
Alexander Cristurean <acristur@redhat.com>
2026-03-10 16:17:05 +0100

    fix: cosmetic changes.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- make/tools.mk
+++ make/tools.mk
@@ -2,10 +2,10 @@
 ##@ Testsuite Tools
 
 .PHONY: deploy-testsuite-tools
-deploy-testsuite-tools: ## Deploy testsuite tools (Keycloak, etc.)
-	@echo "Deploying testsuite tools..."
-	kubectl create namespace tools || true
+deploy-testsuite-tools: ## Deploy testsuite tools (Keycloak, etc.) - requires RH_REGISTRY credentials
 	@if [ -n "$(RH_REGISTRY_USERNAME)" ] && [ -n "$(RH_REGISTRY_PASSWORD)" ]; then \
+		echo "Deploying testsuite tools..."; \
+		kubectl create namespace tools || true; \
 		echo "Creating Red Hat registry secret..."; \
 		kubectl -n tools create secret docker-registry redhat-registry-secret \
 			--docker-server=registry.redhat.io \
@@ -14,16 +14,16 @@ deploy-testsuite-tools: ## Deploy testsuite tools (Keycloak, etc.)
 			--dry-run=client -o yaml | kubectl apply -f -; \
 		kubectl -n tools patch serviceaccount default \
 			-p '{"imagePullSecrets": [{"name": "redhat-registry-secret"}]}'; \
+		helm repo add kuadrant-olm https://kuadrant.io/helm-charts-olm --force-update; \
+		helm repo update; \
+		helm install \
+			--set=tools.keycloak.keycloakProvider=deployment \
+			--set=tools.coredns.enable=false \
+			--debug \
+			--wait \
+			--timeout=$(TOOLS_TIMEOUT) \
+			tools kuadrant-olm/tools-instances; \
+		echo "✅ Testsuite tools deployed"; \
 	else \
-		echo "Red Hat registry credentials not provided, skipping secret creation"; \
+		echo "⏭️  Skipping testsuite tools deployment (requires RH_REGISTRY_USERNAME and RH_REGISTRY_PASSWORD)"; \
 	fi
-	helm repo add kuadrant-olm https://kuadrant.io/helm-charts-olm --force-update
-	helm repo update
-	helm install \
-		--set=tools.keycloak.keycloakProvider=deployment \
-		--set=tools.coredns.enable=false \
-		--debug \
-		--wait \
-		--timeout=$(TOOLS_TIMEOUT) \
-		tools kuadrant-olm/tools-instances
-	@echo "✅ Testsuite tools deployed"
