e4efe50e3fab00ad293d2b45ec7cf2e3a1c51942
Alexander Cristurean <acristur@redhat.com>
2026-03-11 13:46:29 +0100

    feat: added env patch for kuadrant-operator.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- make/kuadrant.mk
+++ make/kuadrant.mk
@@ -20,6 +20,7 @@ else
 		helm install kuadrant-operator kuadrant/kuadrant-operator --create-namespace --namespace $(KUADRANT_NAMESPACE), \
 		helm install kuadrant-operator kuadrant/kuadrant-operator --version $(KUADRANT_OPERATOR_VERSION) --create-namespace --namespace $(KUADRANT_NAMESPACE))
 	kubectl -n $(KUADRANT_NAMESPACE) wait --timeout=$(KUBECTL_TIMEOUT) --for=condition=Available deployments --all
+	$(MAKE) patch-kuadrant-operator-env
 	@echo "✅ Kuadrant Operator $(KUADRANT_OPERATOR_VERSION) installed"
 endif
 
@@ -44,7 +45,30 @@ deploy-kuadrant-operator-local: ## Deploy Kuadrant Operator from local build/ima
 		kustomize edit set image controller=$(KUADRANT_OPERATOR_IMAGE) && \
 		kustomize build ../deploy | kubectl apply --server-side -f -
 	kubectl -n $(KUADRANT_NAMESPACE) wait --timeout=$(KUBECTL_TIMEOUT) --for=condition=Available deployments --all
+	$(MAKE) patch-kuadrant-operator-env
 	@echo "✅ Kuadrant Operator deployed from image $(KUADRANT_OPERATOR_IMAGE)"
+
+.PHONY: patch-kuadrant-operator-env
+patch-kuadrant-operator-env: ## Patch Kuadrant Operator deployment with custom env vars
+ifneq ($(KUADRANT_OPERATOR_ENV_VARS),)
+	@echo "Patching Kuadrant Operator with environment variables..."
+	@ENV_PATCH='['; \
+	IFS=',' read -ra PAIRS <<< "$(KUADRANT_OPERATOR_ENV_VARS)"; \
+	for i in "$${!PAIRS[@]}"; do \
+		PAIR="$${PAIRS[$$i]}"; \
+		NAME=$$(echo "$$PAIR" | cut -d'=' -f1); \
+		VALUE=$$(echo "$$PAIR" | cut -d'=' -f2-); \
+		[ $$i -gt 0 ] && ENV_PATCH="$$ENV_PATCH,"; \
+		ENV_PATCH="$$ENV_PATCH{\"name\":\"$$NAME\",\"value\":\"$$VALUE\"}"; \
+	done; \
+	ENV_PATCH="$$ENV_PATCH]"; \
+	kubectl patch deployment kuadrant-operator-controller-manager -n $(KUADRANT_NAMESPACE) \
+		--type=json -p="[{\"op\":\"add\",\"path\":\"/spec/template/spec/containers/0/env\",\"value\":$$ENV_PATCH}]"; \
+	kubectl -n $(KUADRANT_NAMESPACE) rollout status deployment/kuadrant-operator-controller-manager --timeout=$(KUBECTL_TIMEOUT)
+	@echo "✅ Kuadrant Operator patched with env vars"
+else
+	@echo "No custom env vars specified (KUADRANT_OPERATOR_ENV_VARS not set)"
+endif
 
 .PHONY: deploy-kuadrant-cr
 deploy-kuadrant-cr: ## Deploy Kuadrant CR
