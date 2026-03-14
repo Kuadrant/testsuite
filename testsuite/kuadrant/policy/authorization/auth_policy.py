010625bef5c1817e332ce00ca3a5b6084c31ab29
Alexander Cristurean <acristur@redhat.com>
2026-03-14 22:56:07 +0100

    fix: rework spec section in auth/rl policies.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/kuadrant/policy/authorization/auth_policy.py
+++ testsuite/kuadrant/policy/authorization/auth_policy.py
@@ -55,15 +55,27 @@ class AuthPolicy(Policy, AuthConfig):
         if self.spec_section is None:
             raise TypeError("Strategy can only be set on defaults or overrides")
 
-        self.spec_section["strategy"] = strategy.value
+        section = self.spec_section
+        if isinstance(self.spec_section, str):
+            # String marker - create the section now
+            section = self.model.spec.setdefault(self.spec_section, {})
+
+        section["strategy"] = strategy.value
         self.spec_section = None
 
     @property
     def auth_section(self):
+        """Returns the rules section for adding auth configuration"""
         if self.spec_section is None:
-            self.spec_section = self.model.spec
+            # Implicit mode - use model.spec directly
+            spec_section = self.model.spec
+        elif isinstance(self.spec_section, str):
+            # String marker ("defaults" or "overrides") - create the section now
+            spec_section = self.model.spec.setdefault(self.spec_section, {})
+        else:
+            # Already a dict (shouldn't happen with new code but keep for compatibility)
+            spec_section = self.spec_section
 
-        spec_section = self.spec_section
         self.spec_section = None
         return spec_section.setdefault("rules", {})
 
@@ -75,13 +87,17 @@ class AuthPolicy(Policy, AuthConfig):
     @property
     def defaults(self):
         """Add new rule into the `defaults` AuthPolicy section"""
-        self.spec_section = self.model.spec.setdefault("defaults", {})
+        # Don't create the dict yet - only mark which section to use
+        # The dict will be created when auth_section is called
+        self.spec_section = "defaults"
         return self
 
     @property
     def overrides(self):
         """Add new rule into the `overrides` AuthPolicy section"""
-        self.spec_section = self.model.spec.setdefault("overrides", {})
+        # Don't create the dict yet - only mark which section to use
+        # The dict will be created when auth_section is called
+        self.spec_section = "overrides"
         return self
 
     @modify
