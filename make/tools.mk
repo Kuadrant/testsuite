0ce6ea739b3e42f5387ba7991e04085e3e16b441
Alexander Cristurean <acristur@redhat.com>
2026-04-06 16:26:06 +0200

    feat: add Jaeger tracing to local-setup
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>
    
    # Conflicts:
    #	config/settings.local.yaml.tpl

--- make/tools.mk
+++ make/tools.mk
@@ -3,11 +3,12 @@
 
 .PHONY: deploy-testsuite-tools
 deploy-testsuite-tools: ## Deploy testsuite tools (Keycloak, etc.)
-	@echo "Deploying testsuite tools..."
-	kubectl create namespace tools || true
+	@echo "Deploying testsuite tools to namespace: $(TOOLS_NAMESPACE)"
+	kubectl create namespace $(TOOLS_NAMESPACE) || true
 	helm repo add kuadrant-olm https://kuadrant.io/helm-charts-olm --force-update
 	helm repo update
 	helm install \
+		--namespace $(TOOLS_NAMESPACE) \
 		--set=tools.keycloak.keycloakProvider=deployment \
 		--debug \
 		--wait \
