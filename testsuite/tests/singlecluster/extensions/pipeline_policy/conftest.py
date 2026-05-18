40c888a2e7b79a73462c1530b62fbe0b4a31208f
Alexander Cristurean <acristur@redhat.com>
2026-05-18 11:35:35 +0200

    fix: removed authorization feature.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/tests/singlecluster/extensions/pipeline_policy/conftest.py
+++ testsuite/tests/singlecluster/extensions/pipeline_policy/conftest.py
@@ -4,14 +4,6 @@ import pytest
 
 from testsuite.kuadrant.extensions.pipeline_policy import PipelinePolicy
 
-# no wasm plugin gets created without having
-@pytest.fixture(scope="module")
-def authorization(authorization):
-    """Setup AuthConfig for test"""
-    authorization.identity.add_anonymous("anonymous")
-    authorization.responses.add_simple("auth.identity.anonymous")
-    return authorization
-
 @pytest.fixture(scope="module")
 def pipeline_policy(cluster, blame, route):
     """PipelinePolicy targeting the test HTTPRoute"""
@@ -19,9 +11,9 @@ def pipeline_policy(cluster, blame, route):
 
 
 @pytest.fixture(scope="module", autouse=True)
-def commit(request, authorization, pipeline_policy):
+def commit(request, pipeline_policy):
     """Commit and wait for PipelinePolicy to be ready."""
-    for component in [authorization, pipeline_policy]:
+    for component in [pipeline_policy]:
         request.addfinalizer(component.delete)
         component.commit()
         component.wait_for_ready()
