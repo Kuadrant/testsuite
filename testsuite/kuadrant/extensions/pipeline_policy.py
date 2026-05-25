eba7c2555a92e06895928de17bf45be9cdbe8e41
Alexander Cristurean <acristur@redhat.com>
2026-05-25 16:19:19 +0200

    feat: updated pipeline extensions with updates from the sdk.
    Signed-off-by: Alexander Cristurean <acristur@redhat.com>

--- testsuite/kuadrant/extensions/pipeline_policy.py
+++ testsuite/kuadrant/extensions/pipeline_policy.py
@@ -48,43 +48,93 @@ class PipelinePolicy(Policy):
         )
 
     @modify
-    def add_request_allow(self, intention: str, predicate: Optional[str] = None):
-        """Add an allow request action with a CEL predicate"""
-        action: Dict = {"type": "allow", "intention": intention}
-        if predicate:
-            action["predicate"] = predicate
-        self.model.spec.setdefault("request", []).append(action)
-
-    @modify
-    def add_request_grpc_method(
-        self,
-        method: str,
-        var: Optional[str] = None,
-        intention: Optional[str] = None,
-        predicate: Optional[str] = None,
-    ):
+    def add_request_grpc_method(self, method: str, var: Optional[str] = None, predicate: Optional[str] = None):
         """Add a grpc_method request action that calls an upstream"""
         action: Dict = {"type": "grpc_method", "method": method}
         if var:
             action["var"] = var
-        if intention:
-            action["intention"] = intention
         if predicate:
             action["predicate"] = predicate
         self.model.spec.setdefault("request", []).append(action)
 
     @modify
-    def add_response_headers(self, headers: List[List[str]], predicate: Optional[str] = None):
-        """Add an add_headers response action"""
+    def add_request_deny(
+        self,
+        predicate: Optional[str] = None,
+        with_status: Optional[int] = None,
+        with_headers: Optional[str] = None,
+        with_body: Optional[str] = None,
+    ):
+        """Add a deny request action"""
+        action: Dict = {"type": "deny"}
+        if predicate:
+            action["predicate"] = predicate
+        if with_status:
+            action["withStatus"] = with_status
+        if with_headers:
+            action["withHeaders"] = with_headers
+        if with_body:
+            action["withBody"] = with_body
+        self.model.spec.setdefault("request", []).append(action)
+
+    @modify
+    def add_request_fail(self, log_message: str, predicate: Optional[str] = None):
+        """Add a fail request action"""
+        action: Dict = {"type": "fail", "logMessage": log_message}
+        if predicate:
+            action["predicate"] = predicate
+        self.model.spec.setdefault("request", []).append(action)
+
+    @modify
+    def add_request_headers(self, headers: List[List[str]], predicate: Optional[str] = None):
+        """Add an add_headers request action"""
         action: Dict = {"type": "add_headers", "headersToAdd": str(headers)}
+        if predicate:
+            action["predicate"] = predicate
+        self.model.spec.setdefault("request", []).append(action)
+
+    @modify
+    def add_response_grpc_method(self, method: str, var: Optional[str] = None, predicate: Optional[str] = None):
+        """Add a grpc_method response action that calls an upstream"""
+        action: Dict = {"type": "grpc_method", "method": method}
+        if var:
+            action["var"] = var
         if predicate:
             action["predicate"] = predicate
         self.model.spec.setdefault("response", []).append(action)
 
     @modify
-    def add_response_code(self, response_code: int, predicate: Optional[str] = None):
-        """Add a with_response_code response action"""
-        action: Dict = {"type": "with_response_code", "responseCode": response_code}
+    def add_response_deny(
+        self,
+        predicate: Optional[str] = None,
+        with_status: Optional[int] = None,
+        with_headers: Optional[str] = None,
+        with_body: Optional[str] = None,
+    ):
+        """Add a deny response action"""
+        action: Dict = {"type": "deny"}
+        if predicate:
+            action["predicate"] = predicate
+        if with_status:
+            action["withStatus"] = with_status
+        if with_headers:
+            action["withHeaders"] = with_headers
+        if with_body:
+            action["withBody"] = with_body
+        self.model.spec.setdefault("response", []).append(action)
+
+    @modify
+    def add_response_fail(self, log_message: str, predicate: Optional[str] = None):
+        """Add a fail response action"""
+        action: Dict = {"type": "fail", "logMessage": log_message}
+        if predicate:
+            action["predicate"] = predicate
+        self.model.spec.setdefault("response", []).append(action)
+
+    @modify
+    def add_response_headers(self, headers: List[List[str]], predicate: Optional[str] = None):
+        """Add an add_headers response action"""
+        action: Dict = {"type": "add_headers", "headersToAdd": str(headers)}
         if predicate:
             action["predicate"] = predicate
         self.model.spec.setdefault("response", []).append(action)
