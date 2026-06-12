78f7d32073aac08c86e679b4f04e955f606a7fd0
Silvia Tarabova <starabov@redhat.com>
2026-06-12 09:42:53 +0200

    feat(tests): handle clusters without OSSM Istio installation
    Signed-off-by: Silvia Tarabova <starabov@redhat.com>

--- testsuite/tests/singlecluster/gateway/mtls/test_mtls_behaviour.py
+++ testsuite/tests/singlecluster/gateway/mtls/test_mtls_behaviour.py
@@ -4,7 +4,7 @@ Tests the enabling and disabling of mTLS configuration via the Kuadrant CR
 
 import pytest
 
-pytestmark = [pytest.mark.disruptive]
+pytestmark = [pytest.mark.disruptive, pytest.mark.required_ossm]
 
 component_cases = [
     pytest.param(["limitador"], id="limitador-only"),
